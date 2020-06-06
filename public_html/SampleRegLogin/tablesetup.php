7603e58232743e97885aa56c9da6873f73147303
Nick <nak29@njit.edu>
2020-06-05 20:15:46 -0400

    typo in config

--- public_html/SampleRegLogin/tablesetup.php
+++ public_html/SampleRegLogin/tablesetup.php
@@ -9,7 +9,7 @@ try{
 				`email` varchar(100) not null unique,
 				`first_name` varchar(100),
 				`last_name` varchar(100),
-				`password` carchar(60),
+				`password` varchar(60),
 				`created` timestamp default current_timestamp,
 				`modified` timestamp default current_timestamp on update current_timestamp,
 				PRIMARY KEY (`id`)
