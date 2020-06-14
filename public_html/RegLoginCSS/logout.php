6bb90a04cce0b0f8d70c6eed9337ea491105ac3b
Nick <nak29@njit.edu>
2020-06-14 19:41:29 -0400

    removed a var export statement

--- public_html/RegLoginCSS/logout.php
+++ public_html/RegLoginCSS/logout.php
@@ -4,7 +4,7 @@ include("header.php");
 session_unset();
 session_destroy();
 echo "You have been logged out. ";
-echo var_export($_SESSION, true);
+//echo var_export($_SESSION, true);
 //get session cookie and delete/clear it for this session
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
