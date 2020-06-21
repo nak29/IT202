bae0dab27549aec1369cfe18a78b20d0c98b4e52
Nick <nak29@njit.edu>
2020-06-21 13:00:18 -0400

    variable name swapping

---
+++ public_html/TestTables/edit.php
@@ -0,0 +1,63 @@
+<?php
+require("config.php");
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+$db = new PDO($connection_string, $dbuser, $dbpass);
+$thingId = -1;
+$result = array();
+require("common.inc.php");
+if(isset($_GET["thingId"])){
+    $thingId = $_GET["thingId"];
+    $stmt = $db->prepare("SELECT * FROM Products where id = :id");
+    $stmt->execute([":id"=>$thingId]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+else{
+    echo "No product selected for change.";
+}
+?>
+
+    <form method="POST">
+        <label for="thing">New Product
+            <input type="text" id="product" name="product" value="<?php echo get($result, "product");?>" />
+        </label>
+        <label for="p">Price
+            <input type="number" id="q" name="price" value="<?php echo get($result, "price");?>" />
+        </label>
+        <input type="submit" name="updated" value="Update product"/>
+    </form>
+
+<?php
+if(isset($_POST["updated"])){
+    $name = $_POST["product"];
+    $quantity = $_POST["price"];
+    if(!empty($product) && !empty($price)){
+        try{
+            $stmt = $db->prepare("UPDATE Products set name = :product, quantity=:price where id=:id");
+            $result = $stmt->execute(array(
+                ":product" => $product,
+                ":price" => $price,
+                ":id" => $thingId
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo var_export($e, true);
+            }
+            else{
+                echo var_export($result, true);
+                if ($result){
+                    echo "Successfully updated thing: " . $name;
+                }
+                else{
+                    echo "Error updating record";
+                }
+            }
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    else{
+        echo "Product and price must not be empty.";
+    }
+}
+?>
