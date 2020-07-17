c9d532d694f263d147a9697ca3034f1878dfae5b
Nick <nak29@njit.edu>
2020-07-17 11:26:34 -0400

    testing text

--- public_html/MileTwo/home.php
+++ public_html/MileTwo/home.php
@@ -3,4 +3,7 @@ include("header.php");
 
 ?>
     <h4>Home</h4>
+
+<?php echo "<div class=\"welcome\">Welcome " . $_SESSION["user"]["first_name"];"</div>";?>
+
 <?php echo "Welcome " . $_SESSION["user"]["email"];?>
