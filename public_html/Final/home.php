ad806d3f8c1f10d4f6d351b2b905bc3063f4978f
Nick <nak29@njit.edu>
2020-07-17 12:29:22 -0400

    testing text more

--- public_html/Final/home.php
+++ public_html/Final/home.php
@@ -3,4 +3,6 @@ include("header.php");
 
 ?>
     <h4>Home</h4>
-<?php echo "<div class=\"welcome\">Welcome " . $_SESSION["user"]["first_name"];"</div>";?>
+<?php echo "Welcome " . $_SESSION["user"]["email"];?>
+
+<p class="welcome">"Welcome"</p>
--- public_html/MileTwo/home.php
+++ public_html/MileTwo/home.php
@@ -2,5 +2,7 @@
 include("header.php");
 
 ?>
-    <h4>Home</h4>
-    <div class="welcome"><?php echo "Welcome " . $_SESSION["user"]["first_name"];?></div>
+<h4>Home</h4>
+<div class="welcome"><?php echo "Welcome " . $_SESSION["user"]["first_name"];?></div>
+
+<p class="welcome">"Welcome"</p>
