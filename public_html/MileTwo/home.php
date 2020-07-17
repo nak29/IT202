6530f98db9018ec7e2d819379fa265e8a4e0cf81
Nick <nak29@njit.edu>
2020-07-17 11:38:32 -0400

    why doesnt anything ever work

--- public_html/MileTwo/home.php
+++ public_html/MileTwo/home.php
@@ -4,6 +4,5 @@ include("header.php");
 ?>
     <h4>Home</h4>
 
-<?php echo "<div class=\"welcome\">Welcome " . $_SESSION["user"]["first_name"];"</div>";?>
+<?php echo "<div class=\"welcome\">Welcome " . $_SESSION["user"]["first_name"];?>"</div>"
 
-<?php echo "Welcome " . $_SESSION["user"]["email"];?>
