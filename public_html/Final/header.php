fda22e21e84c9221abde76c6157d4d8c7e13b31d
Nick <nak29@njit.edu>
2020-07-18 13:11:45 -0400

    a lot of new stuff!

--- public_html/Final/header.php
+++ public_html/Final/header.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <?php
-require_once("config.php");
+require("config.php");
 session_start();
 ?>
 <nav>
---
+++ public_html/Final/shop.php
@@ -0,0 +1,22 @@
+<?php
+$query = file_get_contents(__DIR__ . "/queries/ASCEND_TABLE_PRODUCTS.sql");
+
+try {
+    $stmt = getDB()->prepare($query);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
+<ul id="shop">
+
+    <?php foreach($results as $row):?>
+        <li>
+            <?php echo get($row, "product")?>
+            <?php echo get($row, "price");?>
+            <?php echo get($row, "quantity");?>
+            <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
+        </li>
+    <?php endforeach;?>
+</ul>
