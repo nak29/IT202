2b4363859ff7421e78701700fe57a0008d231949
Nick <nak29@njit.edu>
2020-07-18 19:23:10 -0400

    removing         require_once("config.php");

--- public_html/MileTwo/common.inc.php
+++ public_html/MileTwo/common.inc.php
@@ -8,7 +8,6 @@ function get($arr, $key){
 function getDB(){
     global $db;
     if(!isset($db)) {
-        require_once("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         $db = new PDO($connection_string, $dbuser, $dbpass);
     }
