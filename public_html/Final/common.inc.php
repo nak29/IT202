a90805f68870857ef29b2e2a43c855042d32972a
Nick <nak29@njit.edu>
2020-07-18 23:58:39 -0400

    better

--- public_html/Final/common.inc.php
+++ public_html/Final/common.inc.php
@@ -1,4 +1,6 @@
 <?php
+require_once("config.php");
+
 function get($arr, $key){
     if(isset($arr[$key])){
         return $arr[$key];
@@ -8,7 +10,6 @@ function get($arr, $key){
 function getDB(){
     global $db;
     if(!isset($db)) {
-        require_once("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         $db = new PDO($connection_string, $dbuser, $dbpass);
     }
--- public_html/Final/header.php
+++ public_html/Final/header.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <?php
-require("config.php");
+require("common.inc.php");
 session_start();
 ?>
 <nav>
