841c11579a4f01a145ab71b56fbf14aba4b4d472
Nick <nak29@njit.edu>
2020-07-19 00:45:14 -0400

    test

--- public_html/Final/login.php
+++ public_html/Final/login.php
@@ -20,6 +20,13 @@ if(isset($_POST["login"])){
     if(isset($_POST["password"]) && isset($_POST["email"])){
         $password = $_POST["password"];
         $email = $_POST["email"];
+
+        $cleardb_url = parse_url(getenv("JAWSDB_URL"));
+        $dbhost = $cleardb_url["host"];
+        $dbuser = $cleardb_url["user"];
+        $dbpass = $cleardb_url["pass"];
+        $dbdatabase = substr($cleardb_url["path"], 1);
+
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
