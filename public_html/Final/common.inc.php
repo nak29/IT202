14991660a310afe82edc97d1ae9299e9e967b6bf
Nick <nak29@njit.edu>
2020-07-19 01:14:45 -0400

    testing more

--- public_html/Final/common.inc.php
+++ public_html/Final/common.inc.php
@@ -12,6 +12,11 @@ function getDB(){
 
     global $db;
     if(!isset($db)) {
+        $cleardb_url      = parse_url(getenv("JAWSDB_URL"));
+        $dbhost   = $cleardb_url["host"];
+        $dbuser = $cleardb_url["user"];
+        $dbpass = $cleardb_url["pass"];
+        $dbdatabase       = substr($cleardb_url["path"],1);
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         $db = new PDO($connection_string, $dbuser, $dbpass);
     }
