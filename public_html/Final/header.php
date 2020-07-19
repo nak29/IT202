dea7f1642df05d46270f926b3cbed37d3892a65e
Nick <nak29@njit.edu>
2020-07-19 00:53:27 -0400

    fibckjshut up

--- public_html/Final/header.php
+++ public_html/Final/header.php
@@ -5,6 +5,11 @@
 <?php
 error_reporting(-1);
 ini_set('display_errors', 1);
+$cleardb_url = parse_url(getenv("JAWSDB_URL"));
+$dbhost = $cleardb_url["host"];
+$dbuser = $cleardb_url["user"];
+$dbpass = $cleardb_url["pass"];
+$dbdatabase = substr($cleardb_url["path"], 1);
 include("common.inc.php");
 session_start();
 ?>
--- public_html/Final/login.php
+++ public_html/Final/login.php
@@ -21,12 +21,6 @@ if(isset($_POST["login"])){
         $password = $_POST["password"];
         $email = $_POST["email"];
 
-        $cleardb_url = parse_url(getenv("JAWSDB_URL"));
-        $dbhost = $cleardb_url["host"];
-        $dbuser = $cleardb_url["user"];
-        $dbpass = $cleardb_url["pass"];
-        $dbdatabase = substr($cleardb_url["path"], 1);
-
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
