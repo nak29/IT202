cac287c763708c53f2c15bbfd686b7495012613f
Nick <nak29@njit.edu>
2020-07-19 00:10:47 -0400

    echo test 2

--- public_html/Final/config.php
+++ public_html/Final/config.php
@@ -1,6 +1,6 @@
 <?php
 $cleardb_url      = parse_url(getenv("JAWSDB_URL"));
-echo $cleardb_url;
+echo(print_r($cleardb_url));
 $dbhost   = $cleardb_url["host"];
 $dbuser = $cleardb_url["user"];
 $dbpass = $cleardb_url["pass"];
