ef468e5e2b0824481553c2e6844e15090df8e1f8
Nick <nak29@njit.edu>
2020-07-19 00:14:35 -0400

    dfgh

--- public_html/Final/common.inc.php
+++ public_html/Final/common.inc.php
@@ -1,5 +1,5 @@
 <?php
-include("config.php");
+/*include("config.php");*/
 
 function get($arr, $key){
     if(isset($arr[$key])){
@@ -8,6 +8,13 @@ function get($arr, $key){
     return "";
 }
 function getDB(){
+
+    $cleardb_url = parse_url(getenv("JAWSDB_URL"));
+    $dbhost = $cleardb_url["host"];
+    $dbuser = $cleardb_url["user"];
+    $dbpass = $cleardb_url["pass"];
+    $dbdatabase = substr($cleardb_url["path"], 1);
+
     global $db;
     if(!isset($db)) {
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
