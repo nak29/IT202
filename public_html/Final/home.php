7a8f7f8453144582d1d65b9817548c6c3a99406d
Nick <nak29@njit.edu>
2020-07-24 14:23:46 -0400

    better welcome message

--- public_html/Final/home.php
+++ public_html/Final/home.php
@@ -4,7 +4,7 @@ include(__DIR__ . "/header.php");
 ?>
 <h4>Home</h4>
 <?php if (isset($_SESSION["user"])):?>
-    <p class="welcome"><?php echo "Welcome " . $_SESSION["user"]["first_name"];?></p>
+    <p class="welcome"><?php echo "Welcome " . $_SESSION["user"]["first_name"] . " " . $_SESSION["user"]["last_name"] ."!";?></p>
 <?php else:?>
-    <p class="welcome"><?php echo "Welcome new user";?></p>
+    <p class="welcome"><?php echo "Welcome new user!";?></p>
 <?php endif;?>
