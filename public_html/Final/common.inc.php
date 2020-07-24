ad53b948a629a3be59ac02a4128067d6e5a376c7
Nick <nak29@njit.edu>
2020-07-23 20:27:43 -0400

    revert

--- public_html/Final/common.inc.php
+++ public_html/Final/common.inc.php
@@ -3,15 +3,7 @@ require(__DIR__ . "/config.php");
 
 
 function get($arr, $key){
-    if(!isset($db)) {
-        $cleardb_url      = parse_url(getenv("JAWSDB_URL"));
-        $dbhost   = $cleardb_url["host"];
-        $dbuser = $cleardb_url["user"];
-        $dbpass = $cleardb_url["pass"];
-        $dbdatabase       = substr($cleardb_url["path"],1);
-        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-        $db = new PDO($connection_string, $dbuser, $dbpass);
-    }
+
     if(isset($arr[$key])){
         return $arr[$key];
     }
