ee598d6191c4bf6fb428d1449999bdb0c6880673
Nick <nak29@njit.edu>
2020-07-24 15:59:36 -0400

    near-finished tables

--- public_html/MyTables/CREATE_TABLE_CART.sql
+++ public_html/MyTables/CREATE_TABLE_CART.sql
@@ -3,10 +3,10 @@ CREATE TABLE Cart(
     product_id int,
     quantity int,
     user_id int,
-    total decimal(10,2),
+    subtotal decimal(10,2),
     created datetime default current_timestamp ,
     modified datetime default current_timestamp on update current_timestamp,
     primary key(id),
-    foreign key(product_id) references Products.id,
-    foreign key(user_id) references Users.id
+    foreign key(product_id) references Products(id),
+    foreign key(user_id) references Users(id)
 )
--- public_html/MyTables/CREATE_TABLE_ORDERS.sql
+++ public_html/MyTables/CREATE_TABLE_ORDERS.sql
@@ -1,13 +1,14 @@
 CREATE TABLE Orders(
     id int auto_increment,
+    order_id int,
     product_id int,
     user_id int,
-    cart_id int,
+    quantity int,
     address varchar(120) NOT NULL,
-    created datetime default current_timestamp ,
+    subtotal decimal(10,2),
+    created datetime default current_timestamp,
     modified datetime default current_timestamp on update current_timestamp,
     primary key(id),
-    foreign key(product_id) references Products.id,
-    foreign key(user_id) references Users.id,
-    foreign key(cart_id) references Cart.id
+    foreign key(product_id) references Products(id),
+    foreign key(user_id) references Users(id)
 )
