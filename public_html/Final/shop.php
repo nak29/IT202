8eb1b20067b9752f63e180eadfc726d59c8093a8
Nick <nak29@njit.edu>
2020-07-25 12:24:52 -0400

    edit where current quantity goes

--- public_html/Final/shop.php
+++ public_html/Final/shop.php
@@ -16,7 +16,7 @@ if(isset($_GET["thingId"])) {
     <form method="POST" action="shop.php?thingId=<?php echo get($result, "id")?>">
         <p class="pname"> <?php echo get($result, "product");?> - $<?php echo get($result, "price");?> </p>
 
-        <p class="pdesc"> <?php echo get($result, "description");?> </p>
+        <p class="pdesc"> <?php echo get($result, "description");?> <br><br> Current Quantity: <?php echo get($result, "quantity")?></p>
 
         <input type="submit" name="add" value="Add to cart"/>
 
@@ -95,7 +95,6 @@ if(isset($_GET["thingId"])) {
         <input type="submit" name="remove" value="Remove from cart"/>
 <?php   }?>
 
-        <p>Current Quantity: <?php echo get($checkIfOverZero, "quantity")?></p>
     </form>
 <?php
 }
