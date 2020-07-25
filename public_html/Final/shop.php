c765051781dd0be6e045fe82a7db34d42a3a6ac2
Nick <nak29@njit.edu>
2020-07-25 06:12:27 -0400

    self test

--- public_html/Final/shop.php
+++ public_html/Final/shop.php
@@ -13,7 +13,7 @@ if(isset($_GET["thingId"])) {
     $stmt->execute([":id" => $thingId]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);?>
 
-    <form method="POST">
+    <form action="shop.php">
         <p class="pname"> <?php echo get($result, "product");?> </p>
 
         <p class="pdesc"> <?php echo get($result, "description");?> </p>
