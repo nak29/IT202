1b5d391957117762c72581930180daaa62e0c290
Nick <nak29@njit.edu>
2020-07-25 21:59:04 -0400

    removed connetion stuff again

--- public_html/Final/header.php
+++ public_html/Final/header.php
@@ -4,9 +4,6 @@
 </head>
 <?php
 
-$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-$db = new PDO($connection_string, $dbuser, $dbpass);
-
 //HELPS A WHOLE LOT DONT GET RID OF
 error_reporting(-1);
 ini_set('display_errors', 1);
