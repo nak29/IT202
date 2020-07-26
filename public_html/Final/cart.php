a0d3a33e7e1f1ce07d841b70712b73b29aa6a241
Nick <nak29@njit.edu>
2020-07-25 21:53:25 -0400

    maybe a cart start?

---
+++ public_html/Final/cart.php
@@ -0,0 +1,29 @@
+<?php
+require(__DIR__ . "/header.php");
+
+$user_id = $_SESSION["user"]["id"];
+
+$stmt = $db->prepare("SELECT * FROM Cart where id = :id");
+$stmt->execute([":id" => $user_id]);
+$result = $stmt->fetchall(PDO::FETCH_ASSOC);
+
+?>
+<ul class="cart">
+    <?php foreach($result as $row):?>
+        <?php if(get($row, quantity) > 0) {?>
+            <li>
+                <?php
+                $stmt = $db->prepare("SELECT * FROM Products where id = :id");
+                $stmt->execute([":id" => get($row, product_id)]);
+                $newProduct = $stmt->fetch(PDO::FETCH_ASSOC);
+                ?>
+
+                <?php echo get($newProduct, "product")?>
+                <?php echo get($row, "subtotal");?>
+                <?php echo get($row, "quantity");?>
+                <?php echo get($row, "id");?>
+                <a href="shop.php?thingId=<?php echo get($row, "id");?>">View</a>
+            </li>
+        <?php }?>
+    <?php endforeach;?>
+</ul>
