f91fb581716606b18d9ad138d734d9ac43797beb
Nick <nak29@njit.edu>
2020-07-31 17:14:56 -0400

    this isn't working that's why i am doing so many

--- public_html/Final/orders.php
+++ public_html/Final/orders.php
@@ -22,8 +22,8 @@ $old_created = -1;
         ?><br><?php
         $stmt2 = getDB()->prepare("SELECT * FROM Products where id = :product;");
         $stmt2->execute([":product" => get($row, "product_id")]);
-        $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        print_r($result2);
+        $result2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
         echo get($result2, "product");?>
         <?php echo get($row, "quantity_purchased");
         $old_created = get($row, "created");?>
